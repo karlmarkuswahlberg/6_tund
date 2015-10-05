523de12d9f437120fac4d14152f22af0bac5d6a8
Karl Markus Wahlberg <skmw@greeny.cs.tlu.ee>
2015-10-05 15:25:36 +0300

    can save and change rows

--- table.php
+++ table.php
@@ -9,7 +9,9 @@
 		//saadan kustutatava auto id
 		deleteCarData($_GET["delete"]);
 	}
-	
+	if(isset($_GET["update"])){
+		updateCarData($_GET["car_id"], $_GET["number_plate"], $_GET["color"]);
+	}
 	
 	
 	//saadan return andmed siia. kõik autod objektide kujul massiivis. 
@@ -40,12 +42,16 @@
 		if(isset($_GET["edit"]) && $_GET["edit"] ==  $car_array[$i]->id){
 			
 			echo "<tr>";
+			echo "<form action='table.php' method='get'>";
+			//input mida välja ei näidata. hidden.
+			echo "<input type='hidden' name='car_id' value='".$car_array[$i]->id."'>";
+			
 			echo "<td>".$car_array[$i]->id."</td>";
 			echo "<td>".$car_array[$i]->user_id."</td>";
 			echo "<td><input name='number_plate' value='".$car_array[$i]->number_plate."'></td>";
 			echo "<td><input name='color' value='".$car_array[$i]->color."'></td>";
+			echo "<td><input name='update' type='submit'></td>";
 			echo "<td><a href='table.php'>cancel</a></td>";
-			echo "<td><a href='?updateID=".$car_array[$i]->id."'>salvesta</a></td>";
 			echo"</tr>";
 			
 		}else{
